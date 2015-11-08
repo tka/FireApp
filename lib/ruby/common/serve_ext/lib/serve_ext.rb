081ca577a0f5415749f784f4439b77af47129563
tka.lu <tkatka@gmail.com>
2015-11-09 02:19:54 +0800

    jruby 9.0.3, fix serve_ext

---
+++ lib/ruby/common/serve_ext/lib/serve_ext.rb
@@ -0,0 +1,13 @@
+require 'serve/router'
+require 'serve_ext/router'
+
+require 'serve/rack'
+require 'serve_ext/rack'
+
+require 'serve/view_helpers'
+require 'serve_ext/view_helpers'
+
+require 'serve/pipeline'
+require 'serve_ext/pipeline'
+
+require 'serve'
